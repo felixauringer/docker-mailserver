8a619da9ebb719612729989aefb3a0d445afe38a
Felix Auringer <felix.auringer@protonmail.com>
2022-03-13 09:29:16 +0200

    Disable check for postfix accounts when using oauth2

--- target/scripts/check-for-changes.sh
+++ target/scripts/check-for-changes.sh
@@ -19,7 +19,7 @@ _notify 'task' "${LOG_DATE} Start check-for-changes script."
 cd /tmp/docker-mailserver || exit 1
 
 # check postfix-accounts.cf exist else break
-if [[ ! -f postfix-accounts.cf ]]
+if [[ ! -f postfix-accounts.cf ]] && [[ ${ENABLE_OAUTH2} -eq 0 ]]
 then
   _notify 'inf' "${LOG_DATE} postfix-accounts.cf is missing! This should not run! Exit!"
   exit 0
